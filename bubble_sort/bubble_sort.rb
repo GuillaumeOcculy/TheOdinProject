dff2a95aee377fbf7c13050c1116d3aa987af744
Guillaume Occuly <occuly.guillaume@gmail.com>
2020-05-09 14:07:52 +0200

    Add bubble_sort_by

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -14,3 +14,20 @@ end
 
 p bubble_sort(list)
 # [0, 2, 2, 3, 4, 78]
+
+def bubble_sort_by(array)
+  array.length.times do
+    array.each_with_index do |i, index|
+      next if index + 1 == array.length
+
+      left, right = array[index], array[index + 1]
+      array[index], array[index+1] = right, left if yield(left, right).positive?
+    end
+  end
+  p array
+  # ["hi", "hey", "hello"]
+end
+
+bubble_sort_by(['hi', 'hello', 'hey']) do |left, right|
+  left.length - right.length
+end
